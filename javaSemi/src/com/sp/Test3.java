1ac3f85ca728ad127af906a1333545a128819dcc
stayt <stayt@DRN>
2025-11-06 16:14:32 +0900

    테스트 파일들은 삭제

--- javaSemi/src/com/sp/Test3.java
+++
@@ -1,9 +0,0 @@
-package com.sp;
-
-public class Test3 {
-
-	public static void main(String[] args) {
-		System.out.println("김용현-김=용현");
-	}
-
-}
--- javaSemi/src/com/sp/test2.java
+++
@@ -1,9 +0,0 @@
-package com.sp;
-
-public class test2 {
-
-	public static void main(String[] args) {
-		System.out.println("이준희 수정");
-	}
-
-}
