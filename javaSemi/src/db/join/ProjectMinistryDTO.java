6afe063df21e332e55aa603d7c6938dab34b7964
stayt <stayt@DRN>
2025-11-07 15:23:03 +0900

    과제_협약기관

--- javaSemi/src/db/join/ProjectMinistryDTO.java
+++ javaSemi/src/db/join/ProjectMinistryDTO.java
@@ -1,5 +1,21 @@
 package db.join;
 
-public class ProjectMinistryDTO {
-
+public class ProjectMinistryDTO { //과제_부처테이블
+	private String pCode;
+	private String mCode;
+	
+	public String getpCode() {
+		return pCode;
+	}
+	public void setpCode(String pCode) {
+		this.pCode = pCode;
+	}
+	public String getmCode() {
+		return mCode;
+	}
+	public void setmCode(String mCode) {
+		this.mCode = mCode;
+	}
+	
+	
 }
---
+++ javaSemi/src/db/join/ProjectPartnerOrgDTO.java
@@ -0,0 +1,23 @@
+package db.join;
+
+//과제_협약기관
+public class ProjectPartnerOrgDTO { 
+	private String pCode;
+	private String poCode;
+	
+	public String getpCode() {
+		return pCode;
+	}
+	public void setpCode(String pCode) {
+		this.pCode = pCode;
+	}
+	public String getPoCode() {
+		return poCode;
+	}
+	public void setPoCode(String poCode) {
+		this.poCode = poCode;
+	}
+	
+	
+	
+}
