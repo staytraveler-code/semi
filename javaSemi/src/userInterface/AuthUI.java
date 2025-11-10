082eafbac3c7631a77416552f1c02d1da754078e
sist <sist@DESKTOP-3PP3CQ4>
2025-11-10 17:40:07 +0900

    준희 - 로그인 로직 수정

--- javaSemi/src/userInterface/AuthUI.java
+++ javaSemi/src/userInterface/AuthUI.java
@@ -31,16 +31,10 @@ public class AuthUI {
         try {
             OrganizationDTO org = orgDAO.selectRecord(id); // findById 대신 selectRecord 사용
 
-            if (org == null) {
-                System.out.println("⚠️ 존재하지 않는 아이디입니다.\n");
+            if (org == null || !org.getOrgPwd().equals(pw)) {
+                System.out.println("⚠️ 아이디가 존재하지 않거나, 비밀번호가 틀렸습니다.\n");
                 return;
             }
-
-            if (!org.getOrgPwd().equals(pw)) {
-                System.out.println("⚠️ 비밀번호가 일치하지 않습니다.\n");
-                return;
-            }
-
             System.out.println("✅ 로그인 성공! " + org.getOrgName() + " 기관 환영합니다.\n");
             ui.onOrgLogin(org.getOrgId()); // 로그인 성공 후 기관 ID 전달
 
